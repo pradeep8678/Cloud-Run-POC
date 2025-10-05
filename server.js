ed1c2501fd1299848239bb68ee9cb8c0c23a6f01
Pradip Singh <pradeepsinghania906@gmail.com>
2025-10-05 16:49:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-app.use(bodyParser.json());
-
-// Pub/Sub endpoint
-app.post('/', (req, res) => {
-  const message = req.body.message?.data
-    ? Buffer.from(req.body.message.data, 'base64').toString()
-    : 'No data';
-    
-  console.log(`🚀 Received alert message: ${message}`);
-  res.status(200).send('OK');
+const app = express(); 
+const port = process.env.PORT || 8080;
+app.get('/', (req, res) => { res.send('Hello from Cloud Run! Memory Scaling Project is running.');
 });
-
-// Start server on PORT defined by environment variable
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+app.listen(port, () => { console.log(Web app listening on port ${port}); });
